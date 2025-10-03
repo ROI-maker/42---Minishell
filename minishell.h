374fba92ae02648980be6cd0b2cf4236753fdc2b
ROI-maker <simon.roimaker@gmail.com>
2025-10-03 22:13:19 +0200

    adding

--- minishell.h
+++ minishell.h
@@ -6,27 +6,27 @@
 /*   By: jdupont <jdupont@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:21:19 by jdupont           #+#    #+#             */
-/*   Updated: 2025/10/03 21:21:33 by jdupont          ###   ########.fr       */
+/*   Updated: 2025/10/03 22:13:05 by jdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>      /* Pour printf, perror */
-# include <stdlib.h>     /* Pour malloc, free, exit */
+# include <stdio.h>      /* perror */
+# include <stdlib.h>     /* malloc, free, exit */
 # include <unistd.h>     /* access, open, read, close, fork, execve, dup, dup2, pipe, getcwd, chdir, unlink, isatty, ttyname, ttyslot */
-# include <sys/wait.h>   /* Pour wait, waitpid, wait3, wait4 */
-# include <signal.h>     /* Pour signal, sigaction, sigemptyset, sigaddset, kill */
-# include <sys/stat.h>  /* Pour stat, lstat, fstat */
-# include <sys/ioctl.h>  /* Pour ioctl */
-# include <dirent.h>     /* Pour opendir, readdir, closedir */
-# include <string.h>     /* Pour strerror (bien que perror l'utilise indirectement) */
-# include <termios.h>    /* Pour tcsetattr, tcgetattr */
-# include <term.h>       /* Pour tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (terminfo) */
-# include <readline/readline.h>  /* Pour readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay */
-# include <readline/history.h>   /* Pour add_history */
-# include <fcntl.h>      /* Pour O_RDONLY, O_WRONLY, etc., utilisé avec open */
+# include <sys/wait.h>   /* wait, waitpid, wait3, wait4 */
+# include <signal.h>     /* signal, sigaction, sigemptyset, sigaddset, kill */
+# include <sys/stat.h>  /* stat, lstat, fstat */
+# include <sys/ioctl.h>  /* ioctl */
+# include <dirent.h>     /* opendir, readdir, closedir */
+# include <string.h>     /* strerror (bien que perror l'utilise indirectement) */
+# include <termios.h>    /* tcsetattr, tcgetattr */
+# include <term.h>       /* tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (terminfo) */
+# include <readline/readline.h>  /* readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay */
+# include <readline/history.h>   /* add_history */
+# include <fcntl.h>      /* O_RDONLY, O_WRONLY, etc., */
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <stdbool.h>
@@ -59,5 +59,23 @@
 /******************************************************************************
 *								PROTOTYPES									  *
 ******************************************************************************/
+
+/* ------------------------ BUILTINS ----------------------------------*/
+//cd.c
+void	exemple(void);
+
+//echo.c
+//............
+
+/* ------------------------ EXECUTION ----------------------------------*/
+
+/* ------------------------ INIT ----------------------------------*/
+
+/* ------------------------ PARSING ----------------------------------*/
+
+/* ------------------------ UTILS ----------------------------------*/
+
+
+
 
 #endif
